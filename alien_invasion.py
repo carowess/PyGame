2329daf88833ad72eafe2556ea6514f0a63f219f
Caroline Wescott <caroline_wescott1@baylor.edu>
2021-10-25 15:15:38 -0500

    final game

--- alien_invasion.py
+++ alien_invasion.py
@@ -173,7 +173,7 @@ class AlienInvasion:
                 break
     
     def _change_fleet_direction(self):
-        """Drop the entire fleet adn change the fleet's direction."""
+        """Drop the entire fleet and change the fleet's direction."""
         for alien in self.aliens.sprites():
             alien.rect.y += self.settings.fleet_drop_speed
         self.settings.fleet_direction *= -1
